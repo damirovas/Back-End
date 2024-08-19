cce00f4e5051c42bfd11ac403d79180190af4b2f
damirovas <sdamirova2004@gmail.com>
2024-08-19 19:29:33 +0400

    Task1

---
+++ homeWorks/01.03.2024/Task1/Program.cs
@@ -0,0 +1,76 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region IF ELSE
+            //Console.WriteLine("Operator daxil edin");
+            //string op = Console.ReadLine();
+            //Console.WriteLine("Birinci Ededi daxil edin");
+            //int num1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Ikinci Ededi daxil edin");
+            //int num2 = int.Parse(Console.ReadLine());
+
+            //if (op == "*")
+            //{
+            //    Console.WriteLine(num1 * num2);
+            //}
+            //else if (op == "+")
+            //{
+            //    Console.WriteLine(num1 + num2);
+            //}
+            //else if (op == "-")
+            //{
+            //    Console.WriteLine(num1 - num2);
+            //}
+            //else if (op == "/")
+            //{
+            //    Console.WriteLine(num1 / num2);
+            //}
+            //else if (op == "%")
+            //{
+            //    Console.WriteLine(num1 % num2);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Yanlis operator secimi");
+            //}
+            #endregion
+
+
+            #region SWITCH CASE
+            Console.WriteLine("Operator daxil edin");
+            string op = Console.ReadLine();
+            Console.WriteLine("Birinci Ededi daxil edin");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ikinci Ededi daxil edin");
+            int num2 = int.Parse(Console.ReadLine());
+
+            switch (op)
+            {
+                case "*":
+                    Console.WriteLine(num1 * num2);
+                    break;
+                case "+":
+                    Console.WriteLine(num1 + num2);
+                    break;
+                case "-":
+                    Console.WriteLine(num1 - num2);
+                    break;
+                case "/":
+                    Console.WriteLine(num1 / num2);
+                    break;
+                case "%":
+                    Console.WriteLine(num1 % num2);
+                    break;
+                    default:
+                    Console.WriteLine("Yanlis operator secimi");
+                    break;
+            }
+            #endregion
+        }
+    }
+ }
+
