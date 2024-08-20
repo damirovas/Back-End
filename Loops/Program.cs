f95e751d17a9cdc786420ec150aa35aa2786f441
damirovas <sdamirova2004@gmail.com>
2024-08-20 15:26:51 +0400

    Loops

---
+++ Loops/Program.cs
@@ -0,0 +1,134 @@
+﻿namespace Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region TASK1
+            //int result = 0;
+            //for(int i=0; i<10; i++) 
+            //{ 
+            //     if(i%2==0)
+            //     {
+            //         result += i;
+            //     }
+            //}
+            // Console.WriteLine(result);
+
+            //int result = 0;
+            //int num = 0;
+            //while (num < 10)
+            //{
+            //    num++;
+            //    if(num%2==0)
+            //    {
+            //        result += num;
+            //    }
+            //}
+            //Console.WriteLine(result);
+            #endregion
+
+            #region TASK2
+            //int result = 1;
+            //for(int i = 1; i < 10; i++)
+            //{
+            //    if (i % 2 != 0)
+            //    {
+            //        result *= i;
+            //    }
+            //}
+            //Console.WriteLine(result);
+
+
+            //int result = 1;
+            //int num = 1;
+            //while (num < 10)
+            //{
+            //    num++;
+            //    if(num%2!=0)
+            //    {
+            //        result *= num;
+            //    }
+            //}
+            //Console.WriteLine(result);
+            #endregion
+
+            #region TASK3
+            //int result = 0;
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    if (i == 5 || i == 7) continue;
+            //    result += i;
+            //}
+            //Console.WriteLine(result);
+
+            //int result = 0;
+            //for(int i = 0; i < 10; i++)
+            //{
+            //    if(i!=5 && i!=7)
+            //    {
+            //        result += i;
+            //    }
+            //}
+            //Console.WriteLine(result);
+
+            #endregion
+
+            #region TASK4
+            //int result = 0;
+            //Console.WriteLine("Enter a number");
+            //int num = int.Parse(Console.ReadLine());   
+            //for(int i = 0; i <= num; i++)
+            //{
+            //    result += i;
+            //    if (result > 40) break;
+            //}
+            //Console.WriteLine(result);
+            #endregion
+            #region TASK5
+
+            //Console.WriteLine("Eded daxil edin ");
+            //int num = int.Parse(Console.ReadLine());
+            //int result = 0;
+            //while (num > 0)
+            //{
+            //    result += num % 10;
+            //    num /= 10;
+            //}
+            //Console.WriteLine(result);
+
+            Console.WriteLine("Ededi daxil edin");
+            int num = int.Parse(Console.ReadLine());
+            int result = 0;
+
+            //while(num > 0)
+            //{
+            //    result += num % 10;
+            //    num /= 10;
+            //}
+            //Console.WriteLine(result);
+
+            //for(int i = num; i > 0; i/=10)
+            //{
+            //    result += i % 10;
+            //}
+            //Console.WriteLine(result);
+
+            #endregion
+            #region TASK6
+            //Console.WriteLine("Regem daxil edin");
+            //int num = int.Parse(Console.ReadLine());
+            //int result = 0;
+
+            //while(num > 0)
+            //{
+            //    result = result * 10 + num % 10;
+            //    num/= 10;
+            //}
+            //Console.WriteLine(result);
+
+            #endregion
+        }
+    }
+}
