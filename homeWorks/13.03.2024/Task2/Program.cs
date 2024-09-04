306a14653ef7123bf3080f1e3dc8b1b2dc070cd2
damirovas <sdamirova2004@gmail.com>
2024-09-04 18:27:17 +0400

    ........

---
+++ homeWorks/13.03.2024/Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Stringi daxil et");
+            string str = Console.ReadLine();
+            int max = Int32.MinValue;
+
+            for(int i = 0;i<str.Length;i++)
+            {
+                int count = 0;
+                foreach(char c in str)
+                {
+                    if (c == str[i])
+                    {
+                        count++;
+                    
+                    }
+                }
+               Console.WriteLine($"{str[i]}-{count}");
+            }
+        }
+    }
+}
