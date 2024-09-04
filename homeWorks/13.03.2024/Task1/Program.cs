c22e1546808dabddfc5590da8956ec3ce79d5ff9
damirovas <sdamirova2004@gmail.com>
2024-09-04 18:04:22 +0400

    ......

---
+++ homeWorks/13.03.2024/Task1/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("agacin uzunlugunu geyd et");
+            //int agac = int.Parse(Console.ReadLine());
+
+            //for (int i = 0; i < agac; i++)
+            //{
+            //    for (int j = 0; j < agac - i - 1; j++)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    for (int s = 0; s < 2 * i + 1; s++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            Console.WriteLine("agacin uzunlugunu daxil edin");
+            int num = int.Parse(Console.ReadLine());    
+            for(int i = 0; i < num; i++)
+            {
+                for(int j = 0; j < num-i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for(int s= 0; s < i+1; s++)
+                {
+                    Console.Write(" *");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+ }
+
+
+
