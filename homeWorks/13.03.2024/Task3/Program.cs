88d345e1f823b6c48f6b0dadd464f756cd315cdc
damirovas <sdamirova2004@gmail.com>
2024-09-04 18:32:36 +0400

    .......

---
+++ homeWorks/13.03.2024/Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Stringi daxil et");
+            string str = Console.ReadLine();
+
+            int count = 0;
+
+            foreach(char c in str)
+            {
+                if(c ==' ')
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine($"cumlede {count+1} soz var");
+        }
+    }
+}
