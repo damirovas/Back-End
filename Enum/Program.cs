e7b7b57afd46330eec9a60b8735967cd5a1c92b3
damirovas <sdamirova2004@gmail.com>
2024-09-04 14:06:32 +0400

    ....

---
+++ Enum/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Enum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine((int)OrderStatus.Pending);
+            Console.WriteLine((OrderStatus)100);
+        }
+        enum OrderStatus 
+        {
+            Pending=100,
+            Cargo,
+            Delivered,
+            Rejected
+        }
+
+    }
+    internal class Student 
+    {
+        public string Name {  get; set; }
+        public string LastName { get; set; }
+        public Student(string name, string lastName)
+        {
+            Name = name;
+
+            LastName = lastName;
+
+        }
+    }
+
+}
