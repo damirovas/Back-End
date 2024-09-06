bfaa577dfbce90db2088f06fc1c941e79471cdb7
damirovas <sdamirova2004@gmail.com>
2024-09-06 10:11:53 +0400

    .......

---
+++ homeWorks/13.03.2024/Task5/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Task5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt1 = new DateTime(2004, 07, 2);
+            DateTime dt2 = DateTime.Now;
+            int dayDiff = int.Parse((dt2 - dt1).ToString("dd"));
+            int year = dayDiff / 365;
+            Console.WriteLine(year);
+        }
+    }
+}
