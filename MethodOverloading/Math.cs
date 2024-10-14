125d0121e8f2655f1f0856fb33d6339b6c73d518
damirovas <sdamirova2004@gmail.com>
2024-10-14 15:59:06 +0400

    ......

---
+++ MethodOverloading/Math.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading
+{
+    internal class Math
+    {
+       public int IndexOff(string Str ,char ch)
+        {
+            int index = -1;
+            for(int i = 0; i < Str.Length; i++)
+            {
+                if (Str[i] == ch)
+                {
+                    index = i;
+                    break;
+                }
+            }
+            return index;
+        }
+    }
+}
---
+++ MethodOverloading/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace MethodOverloading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         Math math = new Math();
+            Console.WriteLine(math.IndexOff("sema",'a'));
+        }
+    }
+}
