8c6758dae27c6edf7910cea17af30d0e87eb548c
damirovas <sdamirova2004@gmail.com>
2024-10-30 16:59:48 +0400

    .....

---
+++ Taskk/Group.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taskk
+{
+    internal class Group
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Limit { get; set; }
+        public Student[] students = { };
+
+        public Group(int id, string name, int limit)
+        {
+            Id = id;
+            Name = name;
+            Limit = limit;
+        }
+        public void AddStudent(Student student)
+        {
+            if (students.Length < Limit)
+            {
+                Array.Resize(ref students, students.Length + 1);
+                students[students.Length - 1] = student;
+            }else
+            {
+                Console.WriteLine(" Bu grupda yerler dolub :(((");
+            }
+
+        }
+        public void RemoveStudent(int StudentId)
+        {
+            Student[] newStudent = { };
+            Student removedStudent = null;
+            foreach (Student st in students)
+            {
+                if (st.Id != Id)
+                {
+                    Array.Resize( ref newStudent, newStudent.Length + 1);
+                    newStudent[newStudent.Length - 1] = st;
+                }
+                else
+                {
+                    removedStudent= st;
+
+                }
+                students= newStudent;
+                removedStudent = st;
+                Console.WriteLine(removedStudent + " is removed");
+
+
+            }
+        }
+        public void UpdateStudent(int id,Student student)
+        {
+            foreach (Student stu in students)
+            {
+                if(stu.Id != Id)
+                {
+                    stu.Id = Id;
+                    stu.Name = student.Name;
+                    stu.LastName = student.LastName;
+                    return;
+                }
+            }
+            Console.WriteLine("Bu id ile telebe tapilmadi");
+
+        }
+        public void GetStudents()
+        {
+            foreach(Student st in students)
+            {
+                Console.WriteLine(st);
+            }
+        }
+    }
+}
---
+++ Taskk/Program.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Concurrent;
+
+namespace Taskk
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = EnterChoosens();
+            Group group = null;
+
+            while(result!=0)
+            {
+                switch (result)
+                {
+                    case 1:
+                        group = EnterGroupDetails();
+                        result=EnterChoosens();
+                        break;
+                     case 2:
+                        Student student = EnterStudentDetails();  
+                        group?.AddStudent(student);
+                        result= EnterChoosens();
+                        break;
+                    case 3:
+                        Console.WriteLine("Silinicek telebenin id sini yazin");
+                        int id = int.Parse(Console.ReadLine());
+                        group?.RemoveStudent(id);
+                        result = EnterChoosens();
+                        break;
+                    case 4:
+                        Console.WriteLine("edit olunacaq telebenin id sini yaz");
+                        int updateid = int.Parse((Console.ReadLine()));
+                        Student updateStudent = EnterStudentDetails();
+                        group?.AddStudent(updateStudent);
+                        result = EnterChoosens();
+                        break;
+                        case 5:
+                        group?.GetStudents();
+                        result = EnterChoosens();
+                        break;
+                    default:
+                        Console.WriteLine("Yanlis secim");
+                        result = EnterChoosens();
+                        break;
+
+                }
+            }
+           
+        }
+        static int EnterChoosens()
+        {
+            Console.WriteLine("Press 0 to exit ");
+            Console.WriteLine("Press 1 to add new Group");
+            Console.WriteLine("Press 2 to add new Student");
+            Console.WriteLine("Press 3 to remove Student");
+            Console.WriteLine("Press 4 to edit Student");
+            Console.WriteLine("Press 5 to show added students");
+            int result = int.Parse(Console.ReadLine());
+            return result;
+        }
+        static Group EnterGroupDetails()
+        {
+            Console.WriteLine("Enter Group Id");
+            int id = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Group Name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter Group Limit");
+            int limit = int.Parse(Console.ReadLine());
+
+            Group gr= new Group(id, name, limit);
+            return gr;
+        }
+        static Student EnterStudentDetails()
+        {
+            Console.WriteLine("Enter Student Id ");
+            int id = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Student Name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter Student lastName");
+            string lastname = Console.ReadLine();
+            Student st = new Student(id, name, lastname);
+            return st;
+        }
+    }
+}
